f2769e745dfc07e7551f99dda102bcffec0769d4
Grant <gergely.daniel7@gmail.com>
2024-02-05 01:25:22 +0100

    Add attohttpc

---
+++ http-client/attohttpc-size/src/main.rs
@@ -0,0 +1,24 @@
+use attohttpc::{get, Error};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Ipinfo {
+    pub ip: String,
+    pub hostname: String,
+    pub city: String,
+    pub region: String,
+    pub country: String,
+    pub loc: String,
+    pub org: String,
+    pub postal: String,
+    pub timezone: String,
+    pub readme: String,
+}
+
+fn main() -> Result<(), Error> {
+    let ipinfo: Ipinfo = get("https://ipinfo.io/json").send()?.json()?;
+
+    println!("{}", ipinfo.ip);
+
+    Ok(())
+}
